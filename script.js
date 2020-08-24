6306eecb077f1aa296f438a562fb42de10eefb64
gregorvwk <69797933+gregorvwk@users.noreply.github.com>
2020-08-24 15:59:03 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ let game = function(){
     alert("Klaar op een spelletje te spelen?");
     alert("Top! Je moet het nummer raden tussen 0 en 25");
     alert("Oke succes!");
-    let random = Math.floor(Math.random() * 26);
+    const random = Math.floor(Math.random() * 26);
     let userNumb = prompt(name + " gok maar! " + random);
     let i = 0;
-    while (userNumb = random || i < 5) {
+    while (userNumb != random || i < 5) {
         prompt("Dat is niet correct " + random);
         i++;
     } if (userNumb = random) {
